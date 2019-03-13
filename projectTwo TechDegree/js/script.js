955a5030e40733265240678cb8ef731f42988735
TalWalter <talpaschal@gmail.com>
2019-03-12 18:12:44 -0600

    Add files via upload

--- projectTwo TechDegree/js/script.js
+++ projectTwo TechDegree/js/script.js
@@ -1,45 +1,18 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+// Unit 2 Project TechDegree 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-
+// Select all the 'li' elements with the class 'student-item' and add them to a variable named studentList.
 const studentList = document.querySelectorAll('.student-item');
-const pageDiv =  document.querySelector('.page');
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 
 const showPage = (list, page) => {
+// Loop over items in the list parameter 
   for (let i = 0; i < list.length ; i++) {
+// Determining how many students can be viewed per page    
     const max = (page * 10) -1;
     const base = (page * 10) - 10;
+// Display correct amount of students & hide the rest.  
     if (i >= base && i <= max) {
       list[i].style.display = "block";
     } else {
@@ -48,40 +21,48 @@ const showPage = (list, page) => {
   }
 }
 
-showPage(studentList, 1);
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
 
 const appendPageLinks = (list) => {
-  let totalPages = Math.ceil(studentList.length / 10);
-  let newDiv = document.createElement('div');
+// Determining how pages are needed 
+  const totalPages = Math.ceil(list.length / 10);
+// Create a div that will contain your pagination links
+  const newDiv = document.createElement('div');
   newDiv.className = 'pagination';
+// Select your newDiv's parentNode and append newDiv  
+  const pageDiv =  document.querySelector('.page');
   pageDiv.appendChild(newDiv);
-  
-  let ul = document.createElement('ul');
+// Create the list that will contain your pagination links 
+  const ul = document.createElement('ul');
   newDiv.appendChild(ul);
-
-  for (let i = 0; i < totalPages.length; i ++) {
-    let li = document.createElement('li');
-    let a = document.createElement('a');
-    a.textContent = totalPages[i];
-    ul.appendChild(li);
+// for every page, add li and a tags with the page number text
+  for (i = 1; i <= totalPages; i ++) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.textContent = i + '';
     li.appendChild(a);
+    ul.appendChild(li);
   }
+  
+// Select all of your 'links' (so your 'a' elements) this will help you when calling your eventListner.
+  const aTags = document.querySelectorAll('a');
+// Add an event listener to each a tag, use a for loop inorder to go through all of them 
+  for (let i = 0; i < aTags.length; i ++) {
+    aTags[i].addEventListener('click', (e) => {
+    showPage(studentList, i+1);
+    e.target.className = 'active';
+// remove active class    
+  if (e.target.className === 'active') {
+    aTags[i].classList.remove('active');
+    }
 
+  }); 
+  }
 }
 
-pageDiv.addEventListener('click', (e) => {
-  if (e.target.tagName === 'a') {
-    showPage(studentList, totalPages[i]);
-  }
-});
 
+showPage(studentList, 1);
 appendPageLinks(studentList);
 
 
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
